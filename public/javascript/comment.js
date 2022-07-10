704363665d6dfe4a4247c70fbacb0f1b9c0b313c
Roanan Platt <roanan.platt@gmail.com>
2022-07-10 14:34:48 -0600

    testing

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,11 +1,7 @@
-//async function https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
-//"async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding the need to explicitly configure promise chains." MDN Web Docs
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/async_function name, param, statement
 async function commentFormHandler(event) {
     event.preventDefault();
 
     const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
@@ -21,15 +17,13 @@ async function commentFormHandler(event) {
                 'Content-Type': 'application/json'
             }
         });
-//if else true false boolean "if true return... if false return..."
         if (response.ok) {
             document.location.reload();
         } else {
-            //https://developer.mozilla.org/en-US/docs/Web/API/Response/status
-            //status read-only property of the Response interface contains the status code of the response 
             alert(response.statusText);
         }
     }
 }
-//listen for the submit button 0
+
+
 document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);

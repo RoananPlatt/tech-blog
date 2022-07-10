87f2f90b4cd1210b6a0c0e6b5405377cb07f6b45
Roanan Platt <roanan.platt@gmail.com>
2022-07-10 14:26:13 -0600

    password

--- models/User.js
+++ models/User.js
@@ -4,21 +4,27 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 class User extends Model {
+
     checkPassword(loginPw) {
         return bcrypt.compareSync(loginPw, this.password);
     }
 }
+
+
 User.init({
+
     id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true
     },
+
     username: {
         type: DataTypes.STRING,
         allowNull: false
     },
+
     email: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -27,6 +33,7 @@ User.init({
             isEmail: true
         }
     },
+
     password: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -37,17 +44,16 @@ User.init({
 },
     {
         hooks: {
-            // set up beforeCreate lifecycle "hook" functionality
             async beforeCreate(newUserData) {
                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
                 return newUserData;
             },
-
             async beforeUpdate(updatedUserData) {
                 updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
                 return updatedUserData;
             }
         },
+
         sequelize,
         timestamps: false,
         freezeTableName: true,
@@ -55,4 +61,6 @@ User.init({
         modelName: 'user'
     });
 
+
+    
 module.exports = User;

89c512b38f24cf559ac4c6b625701f1d2b23bf5c
Roanan Platt <roanan.platt@gmail.com>
2022-07-10 14:39:22 -0600

    edit function

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,7 +2,9 @@ async function editFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value;
+
     const post_content = document.querySelector('textarea[name="post-content"]').value.trim();
+
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
@@ -13,16 +15,18 @@ async function editFormHandler(event) {
             title,
             post_content
         }),
+        
         headers: {
             'Content-Type': 'application/json'
         }
     });
-
     if (response.ok) {
         document.location.replace('/dashboard');
     } else {
         alert(response.statusText);
     }
 }
+
+
 
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);

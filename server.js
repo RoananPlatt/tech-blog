85fde01e6a1927274d6522dc63506bda080eb9f9
Roanan Platt <roanan.platt@gmail.com>
2022-07-10 15:06:31 -0600

    testing

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Dependencies
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -6,10 +5,8 @@ const exphbs = require('express-handlebars');
 
 const routes = require('./controllers/');
 const helpers = require('./utils/helpers');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -31,7 +28,6 @@ const hbs = exphbs.create({
 });
 app.engine('handlebars', hbs.engine);
 
-//app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
@@ -40,7 +36,6 @@ app.use(express.urlencoded
         extended: false
     }));
 app.use(express.static(path.join(__dirname, 'public')));
-//path to the routes
 app.use(routes);
 
 sequelize.sync({
@@ -50,5 +45,4 @@ sequelize.sync({
         console.log(`App listening on port ${PORT}!`);
     }
     )
-})
-    ;
+});
